3dae15c3adbc828bba683142e9ea29c4ead28947
pravin <pravin_it14@yahoo.com>
2017-04-25 17:49:56 +0900

    NEW ADD

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,79 @@
+
+    <!-- content ends -->
+    </div><!--/#content.col-md-0-->
+
+</div><!--/fluid-row-->
+
+
+   
+
+    <hr>
+
+    
+    <footer class="row">
+        <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; <a href="#" target="_blank">vipofkk
+               </a> 2017 - <?php echo date('Y') ?></p>
+
+        <p class="col-md-3 col-sm-3 col-xs-12 powered-by">Powered by: <a
+                href="#">spykids</a></p>
+    </footer>
+
+
+</div><!--/.fluid-container-->
+
+<!-- external javascript -->
+
+<script src="{{ asset ('/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+<!-- library for cookie management -->
+<script src="{{ asset ('js/jquery.cookie.js') }}"></script>
+
+
+<!-- calender plugin -->
+<script src="{{ asset ('/bower_components/moment/min/moment.min.js') }} " type="text/javascript"></script>
+<script src="{{ asset ('/bower_components/fullcalendar/dist/fullcalendar.min.js') }}"type="text/javascript"></script>
+
+
+<!-- data table plugin -->
+<script src="{{ asset ('/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
+
+
+
+<!-- select or dropdown enhancer -->
+<script src="{{ asset ('/bower_components/chosen/chosen.jquery.min.js') }}" type="text/javascript"></script>
+
+<!-- plugin for gallery image view -->
+<script src="{{ asset ('/bower_components/colorbox/jquery.colorbox-min.js') }}"type="text/javascript"></script>
+
+<!-- notification plugin -->
+<script src="{{ asset ('/js/jquery.noty.js') }}" type="text/javascript"></script>
+
+
+<!-- library for making tables responsive -->
+<script src="{{ asset ('/bower_components/responsive-tables/responsive-tables.js') }}" type="text/javascript"></script>
+
+<!-- tour plugin -->
+<script src="{{ asset ('bower_components/bootstrap-tour/build/js/bootstrap-tour.min.js') }}" type="text/javascript"></script>
+
+<!-- star rating plugin -->
+<script src="{{ asset ('/js/jquery.raty.min.js') }}" type="text/javascript"></script>
+
+<!-- for iOS style toggle switch -->
+<script src="{{ asset ('/js/jquery.iphone.toggle.js') }}" type="text/javascript"></script>
+
+<!-- autogrowing textarea plugin -->
+<script src="{{ asset ('/js/jquery.autogrow-textarea.js') }}" type="text/javascript"></script>
+
+<!-- multiple file upload plugin -->
+<script src="{{ asset ('js/jquery.uploadify-3.1.min.js') }}" type="text/javascript"></script>
+
+<!-- history.js for cross-browser state change on ajax -->
+<script src="{{ asset ('js/jquery.history.js') }}" type="text/javascript"></script>
+
+<!-- application script for Charisma demo -->
+<script src="{{ asset ('js/charisma.js') }}" type="text/javascript"></script>
+
+
+
+</body>
+</html>
